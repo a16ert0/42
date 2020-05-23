06ff6a68ff0d03805c1a89cef8872db7922b3418
a16ert0 <astrumvia@gmail.com>
2020-05-24 00:37:14 +0300

    4

--- libft/SRCS/ft_strncmp.c
+++ libft/SRCS/ft_strncmp.c
@@ -19,6 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t num)
 	i = 0;
 	if (num == 0 || num + 1 <= 0 || num + 1 < num)
 		return (0);
+	num--;
 	while (s1[i] == s2[i] && num-- && s1[i] != 0 && s2[i] != 0)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
